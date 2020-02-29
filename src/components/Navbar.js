b7df3afb7936d45dbacd0a0473916adf02c725f6
daniel-jain-work <growthharry87@gmail.com>
2020-02-28 19:41:32 -0800

    Make nav item active when match route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,63 +1,86 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
+import { useLocation } from 'react-router-dom';
 import Input from './Input';
 import Link from './Link';
 
-export default () => (
-  <div
-    sx={{
-      display: 'flex',
-      flexDirection: ['column', null, null, 'row'],
-      alignItems: 'center',
-      px: [40, 60, 90, 120],
-      py: 4,
-      boxShadow: 'medium',
-      bg: 'white'
-    }}
-  >
+export default () => {
+  const location = useLocation();
+  return (
     <div
       sx={{
-        flex: 1,
         display: 'flex',
-        flexDirection: ['column', 'row'],
+        flexDirection: ['column', null, null, 'row'],
         alignItems: 'center',
-        width: ['100%', null, null, 'auto']
+        px: [40, 60, 90, 120],
+        py: 4,
+        boxShadow: 'medium',
+        bg: 'white'
       }}
     >
-      <img
-        src={require('../assets/logo.png')}
-        alt="Logo"
-        sx={{ pr: [0, 11], ml: -4 }}
-      ></img>
-      <Input
-        leftComponent={
-          <div sx={{ pr: 2 }}>
-            <i className="fas fa-search"></i>
-          </div>
-        }
-        placeholder="Find Companies & Connections"
-        sx={{ px: 3, flex: 1, mt: [4, 0], width: ['100%', 'auto'] }}
-        inputSx={{ py: 2, fontSize: 2 }}
-      ></Input>
+      <div
+        sx={{
+          flex: 1,
+          display: 'flex',
+          flexDirection: ['column', 'row'],
+          alignItems: 'center',
+          width: ['100%', null, null, 'auto']
+        }}
+      >
+        <img
+          src={require('../assets/logo.png')}
+          alt="Logo"
+          sx={{ pr: [0, 11], ml: -4 }}
+        ></img>
+        <Input
+          leftComponent={
+            <div sx={{ pr: 2 }}>
+              <i className="fas fa-search"></i>
+            </div>
+          }
+          placeholder="Find Companies & Connections"
+          sx={{ px: 3, flex: 1, mt: [4, 0], width: ['100%', 'auto'] }}
+          inputSx={{ py: 2, fontSize: 2 }}
+        ></Input>
+      </div>
+      <div
+        sx={{
+          pl: [0, null, null, 10],
+          pt: [4, null, null, 0],
+          width: ['100%', null, null, 'auto'],
+          flex: 1,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <Link
+          to="/saved-jobs"
+          color={location.pathname === '/saved-jobs' ? 'primary' : 'text'}
+        >
+          JOBS
+        </Link>
+        <Link
+          to="/resumes"
+          color={location.pathname === '/resumes' ? 'primary' : 'text'}
+        >
+          RESUMES
+        </Link>
+        <Link
+          to="/signup"
+          color={
+            ['/signup', '/signin'].includes(location.pathname)
+              ? 'primary'
+              : 'text'
+          }
+        >
+          LOGIN
+        </Link>
+        <i
+          className="fas fa-user-circle"
+          sx={{ fontSize: 5, color: 'border' }}
+        ></i>
+      </div>
     </div>
-    <div
-      sx={{
-        pl: [0, null, null, 10],
-        pt: [4, null, null, 0],
-        width: ['100%', null, null, 'auto'],
-        flex: 1,
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-      }}
-    >
-      <Link>JOBS</Link>
-      <Link>RESUMES</Link>
-      <Link>LOGIN</Link>
-      <i
-        className="fas fa-user-circle"
-        sx={{ fontSize: 5, color: 'border' }}
-      ></i>
-    </div>
-  </div>
-);
+  );
+};
